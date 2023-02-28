c337900278eb1e2b5c2cb09a2c6f2655eef167bf
Ozodimgba <idasiadiachi@gmail.com>
2023-02-28 08:11:18 +0000

    test update

---
+++ tests/auth.controller.spec.ts
@@ -0,0 +1,72 @@
+import request from 'supertest';
+
+import app from '../src/app';
+import mockUser from './mocks/user.mock';
+import prisma from '../src/prisma/prisma';
+import { User } from '../src/libs/types';
+
+describe('Auth - User login', () => {
+  let user: User;
+
+  beforeAll(async () => {
+    await prisma.user.deleteMany();
+  });
+
+  beforeEach(async () => {
+    await prisma.user.deleteMany();
+    // creating user for testing
+    user = await prisma.user.create({
+      data: {
+        email: mockUser.email,
+        password: mockUser.password,
+        name: mockUser.name,
+      },
+    });
+  });
+
+  afterEach(async () => {
+    // deleting created user
+    await prisma.user.deleteMany();
+
+    prisma.$disconnect();
+  });
+
+  it('should return error on using invalid email type', async () => {
+    const response = await request(app).post('/api/v1/auth').send({
+      email: 'email',
+      password: 'password',
+    });
+    expect(response.statusCode).toBe(422);
+  });
+
+  it('should return error on using wrong password', async () => {
+    const response = await request(app).post('/api/v1/auth').send({
+      email: user.email,
+      password: '!password',
+    });
+
+    expect(response.body.message).toMatch(/email or password/);
+    expect(response.statusCode).toBe(400);
+  });
+
+  it('should return error on using wrong email', async () => {
+    const response = await request(app).post('/api/v1/auth').send({
+      email: 'test@email.com',
+      password: 'password',
+    });
+
+    expect(response.body.message).toMatch(/email or password/);
+    expect(response.statusCode).toBe(400);
+  });
+
+  it('should return user token on successful sign in', async () => {
+    const response = await request(app).post('/api/v1/auth').send({
+      email: mockUser.email,
+      password: 'password',
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.status).toBe('success');
+    expect(response.body.data.token).toBeDefined();
+  });
+});
